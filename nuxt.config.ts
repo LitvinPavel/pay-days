07a1947bf148a9a39d25f2622f4d460c97ac9a5f
litvinPV <litvinpv@pba.su>
2024-02-07 09:03:55 +0300

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,9 +30,5 @@ export default defineNuxtConfig({
     public: {
       xmlApi: '',
     }
-  },
-  app: {
-    baseURL: '/pay-days/', // baseURL: '/<repository>/'
-    buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
   }
 })
